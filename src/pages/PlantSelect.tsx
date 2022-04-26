18304ed381cfff36bca6fce345d7836dd838c7e3
Moisés <moisesnetored@gmail.com>
2022-04-26 17:57:28 -0400

    add

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -6,6 +6,7 @@ import { EnvironmentButton } from '../components/EnvironmentButton';
 import { Header } from '../components/Header';
 import { PlantCardPrimary } from '../components/PlantCardPrimary';
 import api from '../services/api';
+import { Load } from '../components/Load';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
@@ -33,6 +34,11 @@ export function PlantSelect(){
   const [plants, setPlants] = useState<PlantProps[]>([]);
   const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>(); // estado auxiliar
   const [environmentSelected, setEnvironmentSelected] = useState('all');
+  const [loading, setLoading] = useState(true);
+
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(true);
+  const [loadedAll, setLoadedAll] = useState(false);
 
   async function fetchEnvironment(){
     const {data} = await api
@@ -49,10 +55,21 @@ export function PlantSelect(){
 
   async function fetchPlants(){
     const {data} = await api
-    .get('plants?_sort=name&_order=asc'); // ordenar
+    .get(`plants?_sort=name&_order=asc&_page=${page}&_limit=4`); // ordenar
 
-    setPlants(data);
-    setFilteredPlants(data);
+    if(!data){
+      return setLoading(true);
+    }
+
+    if(page > 1){
+      setPlants(oldValue => [...oldValue, ...data]); // juntando os dados de antes com os de depois
+    } else {
+      setPlants(data);
+      setFilteredPlants(data);
+    }
+
+    setLoading(false);
+    setLoadingMore(false);
   }
 
   function handleEnvironmentSelected(environment: string){
@@ -69,6 +86,15 @@ export function PlantSelect(){
     setFilteredPlants(filtered);
   }
 
+  function handleFetchMore(distance: number){
+    if(distance < 1)
+      return;
+
+    setLoadingMore(true);
+    setPage(oldValue => oldValue + 1);
+    fetchPlants();
+  }
+
   useEffect(() => { 
     fetchEnvironment();
   }, [])
@@ -76,6 +102,10 @@ export function PlantSelect(){
   useEffect(() => { 
     fetchPlants();
   }, [])
+
+  if(loading){
+    return <Load />
+  }
 
   return(
     <View style={styles.container}>
@@ -97,6 +127,10 @@ export function PlantSelect(){
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.environmentList}
+          onEndReachedThreshold={0.1}
+          onEndReached={({distanceFromEnd}) => 
+            handleFetchMore(distanceFromEnd)
+          }
           renderItem={({item})=> (
             <EnvironmentButton 
               title={item.title}

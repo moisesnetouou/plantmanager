d7015c1e44ad44111474134e82a388959cf71463
Moisés <moisesnetored@gmail.com>
2022-04-27 15:52:42 -0400

    feat: Last adjustments in the application, listening to notifications.

--- App.tsx
+++ App.tsx
@@ -6,15 +6,35 @@ import {
 } from '@expo-google-fonts/jost';
 import { StatusBar } from 'react-native';
 import AppLoading from 'expo-app-loading';
+// import * as Notifications from 'expo-notifications';
 
 
 import Routes from './src/routes'
+// import { PlantProps } from './src/libs/storage';
 
 export default function App(){
   const [ fonstLoaded ] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
   });
+
+  // useEffect(() => {
+    // const subscription = Notifications.addNotificationReceivedListener( // ouvir notificações
+    //   async notification => {
+    //     const data = notification.request.content.data.plant as PlantProps;
+    //     console.log(data);
+    //   }
+    // )
+
+    // return () => subscription.remove();
+    // async function notifications(){ // listar todas notificações
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log("##########NOTIFICAÇÕES###############");
+    //   console.log(data);
+
+    // }
+    // notifications();
+  // }, [])
 
   if(!fonstLoaded){
     return (
---
+++ README.md
@@ -0,0 +1,3 @@
+Precisa ter instalado globalmente o JSON Server.
+
+Para executar o servidor: json-server ./src/services/server.json --host seu_ip --port 3333 --delay 700
